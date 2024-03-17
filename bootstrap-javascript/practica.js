551e91eeea867c5dfc7c85127f6671f7dda65317
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2024-03-16 21:09:43 -0500

    practica js

---
+++ bootstrap-javascript/practica.js
@@ -0,0 +1,10 @@
+function saludar() {
+    var nombre = document.getElementById("nombre").value;
+    console.log("Hola, " + nombre);
+    alert("Hola, " + nombre);
+}
+
+
+let texto = "Curso de programacion web";
+let resultado = text.match(/{a-f}/g);
+alert(resultado);
